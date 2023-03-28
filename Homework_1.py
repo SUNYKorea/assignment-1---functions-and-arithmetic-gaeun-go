fbb70d2dd60dd1066cda64fb0b40df2d698b723c
gaeungo <gaeun.go@stonybrook.edu>
2023-03-28 16:47:20 +0900

    HW1 done!

--- Homework_1.py
+++ Homework_1.py
@@ -1,5 +1,5 @@
-# Name:
-# SBUID: 
+# Name: Gaeun Go
+# SBUID: 115504312
 
 # Remove the ellipses (...) when writing your solutions.
 
@@ -7,11 +7,22 @@
 # ----------------- Convert Fahrenheit to Celsius -------------------------------
 # TODO: Complete the implementation of fahrenheit2celsius () and what_to_wear(). 
 
+from math import tan
+
 def fahrenheit2celsius(fahrenheit): 
-   ...
+    return ((5/9)*(fahrenheit-32))
 
 def what_to_wear(celsius):
-   ...
+    if (celsius<-10):
+        print("Puffy jacket")
+    elif (-10<=celsius) and (celsius<=0):
+        print("Scarf")
+    elif (0<=celsius) and (celsius<=10):
+        print("Sweater")
+    elif (10<=celsius) and (celsius<=20):
+        print("Light jacket")
+    else:
+        print("T-shirt")
 
 # ---------------------------- Exercise II --------------------------------------
 # ----------------- Area and perimeter of a triangle  ---------------------------
@@ -19,14 +30,13 @@ def what_to_wear(celsius):
 # compute_triangle_perimeter from scratch  
 
 def shoelace_triangle_area(x1, y1, x2, y2, x3, y3):
-    ...
+    return abs(((x1*y2+x2*y3+x3*y1)-(x1*y3+x2*y1+x3*y2))/2)
 
 def euclidean_distance(x1, y1, x2, y2):
-    ...
+    return (((x1-x2)**2)+((y1-y2)**2))**(1/2)
 
 def compute_triangle_perimeter(x1, y1, x2, y2, x3, y3):
-    ...
-
+    return (((x1-x2)**2)+((y1-y2)**2))**(1/2)+(((x2-x3)**2)+((y2-y3)**2))**(1/2)+(((x3-x1)**2)+((y3-y1)**2))**(1/2)
 
 # ---------------------------- Exercise III -------------------------------------
 # ----------------- Compute the area of a regular polygon -----------------------
@@ -34,14 +44,18 @@ def compute_triangle_perimeter(x1, y1, x2, y2, x3, y3):
 
 
 def deg2rad(deg):
-    ...
+    pi=3.141592653589793238
+    return (deg*(pi/180))
 
 def apothem(number_sides, length_side):
-   ...
+    n=number_sides
+    s=length_side
+    return (s/(2*tan(deg2rad(180/n))))
 
 def polygon_area(number_sides, length_side):
-   ...
-
+   n=number_sides
+   s=length_side
+   return (n*s*(apothem(n,s))/2)
 
 # ---------------------------- Test -------------------------------------
 # The following lines are for testing purposes, and will not be part of
@@ -61,4 +75,3 @@ print("The area of the triangle is : " + str(area) + " , its perimeter is : " + 
 number_sides = 5
 length_side = 4
 print ("The area of the polygon is : " + str(polygon_area(number_sides, length_side)))
-
